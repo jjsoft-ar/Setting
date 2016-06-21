467d8fbdbeb304440fe4f9717eed9c748fcf5568
Nicolas Widart <n.widart@gmail.com>
2016-06-21 21:46:21 +0200

    Removing usage of the Input facade in favor of function helper

--- Resources/views/admin/fields/translatable/number.blade.php
+++ Resources/views/admin/fields/translatable/number.blade.php
@@ -2,8 +2,8 @@
     {!! Form::label($settingName . "[$lang]", trans($moduleInfo['description'])) !!}
     <?php if (isset($dbSettings[$settingName])): ?>
         <?php $value = $dbSettings[$settingName]->hasTranslation($lang) ? $dbSettings[$settingName]->translate($lang)->value : ''; ?>
-        {!! Form::input('number', $settingName . "[$lang]", Input::old($settingName . "[$lang]", $value), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
+        {!! Form::input('number', $settingName . "[$lang]", old($settingName . "[$lang]", $value), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
     <?php else: ?>
-        {!! Form::input('number', $settingName . "[$lang]", Input::old($settingName . "[$lang]"), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
+        {!! Form::input('number', $settingName . "[$lang]", old($settingName . "[$lang]"), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
     <?php endif; ?>
 </div>
